ae9390101fc6b7a354ef2d7041117a1eb7ae333c
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-04 01:43:47 +0600

    making api for selected chefs

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const chefs = require("./data/chefs.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -10,6 +13,13 @@ app.get("/", (req, res) => {
 
 app.get("/chefs", (req, res) => {
   res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedChefs = chefs.find((n) => n.id == id);
+  res.send(selectedChefs);
 });
 
 app.listen(port, () => {
